7b86fbb0ab8e425835d338a4d9e682d078d81ba1
rbouyaghi2023 <rickos2003@gmail.com>
2024-09-24 11:38:06 +0200

    1commit

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,9 +6,11 @@ use App\Repository\ParticipantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 #[ORM\Entity(repositoryClass: ParticipantRepository::class)]
-class Participant
+class Participant implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -32,6 +34,9 @@ class Participant
 
     #[ORM\Column]
     private ?bool $actif = null;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
 
     #[ORM\OneToMany(mappedBy: 'sortieParticipant', targetEntity: Sortie::class)]
     private Collection $sorties;
@@ -121,36 +126,6 @@ class Participant
         return $this;
     }
 
-    /**
-     * @return Collection<int, Sortie>
-     */
-    public function getSorties(): Collection
-    {
-        return $this->sorties;
-    }
-
-    public function addSorty(Sortie $sorty): static
-    {
-        if (!$this->sorties->contains($sorty)) {
-            $this->sorties->add($sorty);
-            $sorty->setSortieParticipant($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSorty(Sortie $sorty): static
-    {
-        if ($this->sorties->removeElement($sorty)) {
-            // set the owning side to null (unless already changed)
-            if ($sorty->getSortieParticipant() === $this) {
-                $sorty->setSortieParticipant(null);
-            }
-        }
-
-        return $this;
-    }
-
     public function getParticipantCampus(): ?campus
     {
         return $this->participantCampus;
@@ -161,5 +136,47 @@ class Participant
         $this->participantCampus = $participantCampus;
 
         return $this;
+    }
+
+    // Implémentation des méthodes de UserInterface
+
+    /**
+     * Retourne l'identifiant unique de l'utilisateur (par ex. l'adresse e-mail).
+     */
+    public function getUserIdentifier(): string
+    {
+        return $this->mail;  // Utilisation de l'adresse e-mail comme identifiant unique
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // Garantir que chaque utilisateur ait au moins le rôle ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->motPasse;
+    }
+
+    public function getSalt(): ?string
+    {
+        // Si tu utilises bcrypt ou sodium pour hacher les mots de passe, tu n'as pas besoin de salt
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+        // Si tu stockes des données sensibles dans l'entité, tu peux les effacer ici
     }
 }
