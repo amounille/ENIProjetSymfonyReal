17882cdba7b89dd14bb7406c49f31936e8c57fc4
tpohin2023 <theopohin86@gmail.com>
2024-09-27 09:51:11 +0200

    fix migration

--- migrations/Version20240927075047.php
+++ migrations/Version20240927075047.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240924093427 extends AbstractMigration
+final class Version20240927075047 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,8 +23,9 @@ final class Version20240924093427 extends AbstractMigration
         $this->addSql('CREATE TABLE campus (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE etat (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE lieu (id INT AUTO_INCREMENT NOT NULL, lieu_ville_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, rue VARCHAR(255) NOT NULL, latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, INDEX IDX_2F577D5970D356E0 (lieu_ville_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE participant (id INT AUTO_INCREMENT NOT NULL, participant_campus_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, mot_passe VARCHAR(255) NOT NULL, actif TINYINT(1) NOT NULL, roles JSON NOT NULL, INDEX IDX_D79F6B116C5F9AC5 (participant_campus_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE sortie (id INT AUTO_INCREMENT NOT NULL, sortie_lieu_id INT DEFAULT NULL, sortie_etat_id INT DEFAULT NULL, sortie_campus_id INT DEFAULT NULL, sortie_participant_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, date_heure_debut DATETIME NOT NULL, duree TIME NOT NULL, date_limite_inscription DATETIME NOT NULL, nb_inscription_max INT NOT NULL, infos_sortie VARCHAR(255) NOT NULL, etat VARCHAR(255) NOT NULL, INDEX IDX_3C3FD3F2A86218A1 (sortie_lieu_id), INDEX IDX_3C3FD3F2CF8E8D92 (sortie_etat_id), INDEX IDX_3C3FD3F2E8DC5B53 (sortie_campus_id), INDEX IDX_3C3FD3F2480BB42B (sortie_participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE participant (id INT AUTO_INCREMENT NOT NULL, participant_campus_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, mot_passe VARCHAR(255) NOT NULL, actif TINYINT(1) NOT NULL, roles JSON NOT NULL, photo VARCHAR(255) DEFAULT NULL, INDEX IDX_D79F6B116C5F9AC5 (participant_campus_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sortie (id INT AUTO_INCREMENT NOT NULL, sortie_lieu_id INT DEFAULT NULL, sortie_etat_id INT NOT NULL, sortie_campus_id INT DEFAULT NULL, sortie_participant_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, date_heure_debut DATETIME NOT NULL, duree TIME NOT NULL, date_limite_inscription DATETIME NOT NULL, nb_inscription_max INT NOT NULL, infos_sortie VARCHAR(255) NOT NULL, INDEX IDX_3C3FD3F2A86218A1 (sortie_lieu_id), INDEX IDX_3C3FD3F2CF8E8D92 (sortie_etat_id), INDEX IDX_3C3FD3F2E8DC5B53 (sortie_campus_id), INDEX IDX_3C3FD3F2480BB42B (sortie_participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sortie_participant (sortie_id INT NOT NULL, participant_id INT NOT NULL, INDEX IDX_E6D4CDADCC72D953 (sortie_id), INDEX IDX_E6D4CDAD9D1C3019 (participant_id), PRIMARY KEY(sortie_id, participant_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ville (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, code_postal VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE lieu ADD CONSTRAINT FK_2F577D5970D356E0 FOREIGN KEY (lieu_ville_id) REFERENCES ville (id)');
@@ -33,6 +34,8 @@ final class Version20240924093427 extends AbstractMigration
         $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2CF8E8D92 FOREIGN KEY (sortie_etat_id) REFERENCES etat (id)');
         $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2E8DC5B53 FOREIGN KEY (sortie_campus_id) REFERENCES campus (id)');
         $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2480BB42B FOREIGN KEY (sortie_participant_id) REFERENCES participant (id)');
+        $this->addSql('ALTER TABLE sortie_participant ADD CONSTRAINT FK_E6D4CDADCC72D953 FOREIGN KEY (sortie_id) REFERENCES sortie (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE sortie_participant ADD CONSTRAINT FK_E6D4CDAD9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -44,11 +47,14 @@ final class Version20240924093427 extends AbstractMigration
         $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2CF8E8D92');
         $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2E8DC5B53');
         $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2480BB42B');
+        $this->addSql('ALTER TABLE sortie_participant DROP FOREIGN KEY FK_E6D4CDADCC72D953');
+        $this->addSql('ALTER TABLE sortie_participant DROP FOREIGN KEY FK_E6D4CDAD9D1C3019');
         $this->addSql('DROP TABLE campus');
         $this->addSql('DROP TABLE etat');
         $this->addSql('DROP TABLE lieu');
         $this->addSql('DROP TABLE participant');
         $this->addSql('DROP TABLE sortie');
+        $this->addSql('DROP TABLE sortie_participant');
         $this->addSql('DROP TABLE ville');
         $this->addSql('DROP TABLE messenger_messages');
     }
